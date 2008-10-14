1e4aaa843efe4fdfc3bf6757b74ba54809496bff
schyzo99@hotmail.com <schyzo99@hotmail.com@93489655-444f-0410-902b-69a97ebf14b9>
2008-10-14 16:33:53 +0000

    Added functionality : Send an e-mail to the author

--- Rofaje/src/rofage/ihm/menu/MenuAbout.java
+++ Rofaje/src/rofage/ihm/menu/MenuAbout.java
@@ -2,11 +2,13 @@ package rofage.ihm.menu;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
 
 import rofage.common.Engine;
 import rofage.ihm.MainWindow;
 import rofage.ihm.Messages;
 import rofage.ihm.actions.common.ReportBugAction;
+import rofage.ihm.actions.common.SendMailAction;
 import rofage.ihm.actions.common.ShowAboutAction;
 import rofage.ihm.actions.common.ShowUpdateAction;
 
@@ -14,6 +16,7 @@ import rofage.ihm.actions.common.ShowUpdateAction;
 public class MenuAbout extends JMenu {
 	private JMenuItem mItemUpdate 	= null;
 	private JMenuItem mItemReportBug= null;
+	private JMenuItem mItemSendMail	= null;
 	private JMenuItem mItemAbout 	= null;
 	
 	private Engine engine;
@@ -25,10 +28,23 @@ public class MenuAbout extends JMenu {
 		
 		setText("?");
 		add(getMItemUpdate());
+		add(new JSeparator(JSeparator.HORIZONTAL));
 		add(getMItemAbout());
+		add(new JSeparator(JSeparator.HORIZONTAL));
 		add(getMItemReportBug());
+		add(getMItemSendMail());
 	}
 
+	public JMenuItem getMItemSendMail() {
+		if (mItemSendMail==null) {
+			mItemSendMail = new JMenuItem();
+			mItemSendMail.addActionListener(new SendMailAction());
+			mItemSendMail.setText(Messages.getString("SendMail")); //$NON-NLS-1$
+			mItemSendMail.setVisible(true);
+		}
+		return mItemSendMail;
+	}
+	
 	public JMenuItem getMItemReportBug() {
 		if (mItemReportBug==null) {
 			mItemReportBug = new JMenuItem();
