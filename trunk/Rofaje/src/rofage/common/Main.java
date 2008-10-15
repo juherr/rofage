feebcddaf5f42e1eb4c6f0800164d4c71ae1dde7
schyzo99@hotmail.com <schyzo99@hotmail.com@93489655-444f-0410-902b-69a97ebf14b9>
2008-10-15 12:07:23 +0000

    Added functionality : Redirecting standard output to RoFage.log

--- trunk/Rofaje/src/rofage/common/Main.java
+++ trunk/Rofaje/src/rofage/common/Main.java
@@ -1,16 +1,31 @@
 package rofage.common;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Date;
+
 import javax.swing.SwingUtilities;
 
 public class Main {
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable(){
-			public void run() {
-				//new SplashScreen();
-				//new MainWindow();
-				new Engine();
-			}
-		});
+		try {
+			FileOutputStream fos = new FileOutputStream("RoFage.log",true);
+			System.setOut(new PrintStream(fos,true));
+			System.out.println("Starting RoFage... at "+new Date());
+			
+			SwingUtilities.invokeLater(new Runnable(){
+				public void run() {
+					//new SplashScreen();
+					//new MainWindow();
+					new Engine();
+				}
+			});
+			
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
